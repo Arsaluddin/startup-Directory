ab91e96cdf46bb4656b2e3fab32da00cb048d610
arsal uddin <arsaluddin134@gmail.com>
2023-12-16 15:45:36 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,22 @@ const App = () => {
   };
 
   return (
-    <div className="container mx-auto my-8">
-      <FilterDropdown onFilterChange={handleFilterChange} />
-      <StartupList onCardClick={handleCardClick} filter={filter} />
-      {selectedStartup && (
-        <DetailedView startup={selectedStartup} onClose={handleCloseDetailedView} />
-      )}
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center">
+      <header className="bg-blue-500 text-white py-4 w-full">
+        <h1 className="text-3xl font-semibold text-center">Startup Directory</h1>
+      </header>
+      <main className="container mx-auto my-8 flex-grow">
+        <div className="flex justify-center space-x-4">
+          <FilterDropdown onFilterChange={handleFilterChange} />
+        </div>
+        <StartupList onCardClick={handleCardClick} filter={filter} />
+        {selectedStartup && (
+          <DetailedView startup={selectedStartup} onClose={handleCloseDetailedView} />
+        )}
+      </main>
+      <footer className="bg-blue-500 text-white py-2 w-full text-center">
+        <p>&copy; 2023 Startup Directory</p>
+      </footer>
     </div>
   );
 };
